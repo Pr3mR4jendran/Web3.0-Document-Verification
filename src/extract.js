cf07e752c3894864e74c3f0aa23c3c67e5bae0b0
Pr3mR4j <106063722+Pr3mR4j@users.noreply.github.com>
2022-06-24 15:15:21 +0400

    Update extract.js

--- src/extract.js
+++ src/extract.js
@@ -1,8 +1,6 @@
 const hasha = require('hasha');
-
 const extract = async (path) => {
 	const hash = await hasha.fromFile(path, {algorithm: 'sha256'});
 	return hash;
 };
-
 exports.extract = extract;
